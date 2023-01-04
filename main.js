faf1a9c81d16e9f2a29dc9c1beb92f60ae884103
bcstidd <bc.stidd@gmail.com>
2023-01-03 16:20:05 -0800

    EOD 1/3 functions start

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+// console.clear() //clear board
+
+
 const suits = ['d', 'h', 'c', 's']
 const faces = ['A', 'K', 'Q', 'J', '10', '09', '08', '07', '06', '05', '04', '03', '02']
 const deck = []
@@ -5,13 +8,21 @@ const deck = []
 function generateDeck() {
     suits.forEach(suit => {
         faces.forEach(face => {
-            let score
-             if(face === 'K') {
-                 score = 13
-             }
-            // CALCULATE THE SCORE OF THE CARD
-            // AND INCLUDE THAT NUMERICAL VALUE ON THE CARD OBJECT
-            deck.push({
+            let score 
+             if(face === 'A') {'value' === 14}
+             if(face === 'K') {'value' === 13};
+             if(face === 'Q') {'value' === 12};
+             if(face === 'J') {'value' === 11};
+             if(face === '10') {'value' === 10};
+             if(face === '09') {'value' === 09};
+             if(face === '08') {'value' === 08};
+             if(face === '07') {'value' === 07};
+             if(face === '06') {'value' === 06};
+             if(face === '05') {'value' === 05};
+             if(face === '04') {'value' === 04};
+             if(face === '03') {'value' === 03};
+             if(face === '02') {'value' === 02};
+         deck.push({
                 'face': suit + face,
                 'value': score
             })
@@ -27,5 +38,43 @@ function renderDeck() {
     })
 }
 
+// function getCard(){
+//     const randomIndex = getRandom(cards.length);
+//     return cards[randomIndex]
+//   }
+
+// function randomCard() {
+//     console.log(Math.random() * 51);
+// }
+
+
+// getCard ()
+renderDeck()
 generateDeck()
 
+
+
+// const resetHelp = []
+// const instructions = [
+//     'Each player submits one card from their hand of 26 cards. Whichever of the cards has higher value wins the round. First player to retain all 52 cards wins the game.']
+
+// function help () {
+//     document.getElementById('rules').innerHTML = instructions[0];
+// }
+
+// function resetHelp() {
+//     instructions.shift('')
+// }
+
+
+// let compSet, mySet = []
+
+// if(mySet.length === 0 || compSet.length === 0) {
+//     // game over
+//     if( playerDeck.length > 0 ){
+//       console.log('Player Won');
+//     } else {
+//       console.log('CPU Won');
+//     }
+//     return false;
+//   }
