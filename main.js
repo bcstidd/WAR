d507984d105a5ab722f3e85cf26c4a59673015c9
bcstidd <bc.stidd@gmail.com>
2023-01-04 15:35:39 -0800

    New functions after Victor help

--- main.js
+++ main.js
@@ -1,27 +1,56 @@
 // console.clear() //clear board
 
-
+/*----- constants -----*/
 const suits = ['d', 'h', 'c', 's']
 const faces = ['A', 'K', 'Q', 'J', '10', '09', '08', '07', '06', '05', '04', '03', '02']
 const deck = []
+const values = { 
+    'A': 14,
+    'K': 13,
+    'Q': 12,
+    'J': 11,
+    '10': 10,
+    '09': 09,
+    '08': 08,
+    '07': 07,
+    '06': 06,
+    '05': 05,
+    '04': 04,
+    '03': 03,
+    '02': 02,
+};
+const winner = []
+
+// State Variables
+let score, player1Hand, player2Hand 
+
+/*----- cached element references -----*/
+const playBtn = document.getElementById('playBtn')
+const playercard = document.getElementById('playercard')
+const compcard = document.getElementById('compcard')
+const score_p = document.getElementById('score_p')
+const score_c = document.getElementById('score_c')
+
+
+// const boardEl = document.getElementById('board')
+// const resetBtnEl = document.createElement('button')
+
+
+// 
+
+// /*----- event listeners -----*/
+// boardEl.addEventListener('click', handleBoardClick)
+// resetBtnEl.addEventListener('click', handleReset)
+playBtn.addEventListener('click', flipCard)
+newBtn.addEventListener('click', init)
+
+
+/*----- functions -----*/
 
 function generateDeck() {
     suits.forEach(suit => {
         faces.forEach(face => {
-            let score 
-             if(face === 'A') {'value' === 14}
-             if(face === 'K') {'value' === 13};
-             if(face === 'Q') {'value' === 12};
-             if(face === 'J') {'value' === 11};
-             if(face === '10') {'value' === 10};
-             if(face === '09') {'value' === 09};
-             if(face === '08') {'value' === 08};
-             if(face === '07') {'value' === 07};
-             if(face === '06') {'value' === 06};
-             if(face === '05') {'value' === 05};
-             if(face === '04') {'value' === 04};
-             if(face === '03') {'value' === 03};
-             if(face === '02') {'value' === 02};
+            let score = values[face]
          deck.push({
                 'face': suit + face,
                 'value': score
@@ -30,51 +59,66 @@ function generateDeck() {
     })
 }
 
-function renderDeck() {
-    deck.forEach(card => {
-        const cardEl = document.createElement('div')
-        cardEl.className = 'card ' + card.face
-        document.querySelector('body').append(cardEl)
-    })
+
+function shuffleDeck(deck) {
+    for(let i = deck.length - 1; i > 0; i--){
+    const j = Math.floor(Math.random() * (i + 1));
+    const temp = deck[i];
+    deck[i] = deck[j];
+    deck[j] = temp;
+    }
+    return deck;
+    }
+
+function dealHand() {
+    player1Hand = deck.slice(0, 26)
+    player2Hand = deck.slice(26, 52)
 }
 
-// function getCard(){
-//     const randomIndex = getRandom(cards.length);
-//     return cards[randomIndex]
-//   }
+function flipCard() {
+    const player1card = player1Hand.pop()
+    const player2card = player2Hand.pop()
+    playercard.classList[1] ? playercard.classList.replace(playercard.classList[1], player1card.face)  : playercard.classList.add(player1card.face)
+    compcard.classList[1] ? compcard.classList.replace(compcard.classList[1], player2card.face)  : compcard.classList.add(player2card.face)
+    compareCards(player1card, player2card)
 
-// function randomCard() {
-//     console.log(Math.random() * 51);
-// }
+    
+}
+
+function compareCards(player1card, player2card) {
+    if(player1card.value > player2card.value) {
+        console.log("Player 1 Wins this draw!")
+    } else {
+        console.log("The Computer Wins this draw!")
+    }
+renderScore()
+//Whoever wins, push both cards to the bottome of their deck (push to the array)
+//Computed State - 
+
+}
+
+function renderScore() {
+    score_p.innerText= `playercard count ${player1Hand.length}`
+    score_c.innerText= `compcard count ${player2Hand.length}`
+}
+
+function init() {
+    generateDeck()
+    shuffleDeck(deck)
+    dealHand()
+    renderScore()
+    }
+
+init()
 
 
-// getCard ()
-renderDeck()
-generateDeck()
 
 
 
-// const resetHelp = []
-// const instructions = [
-//     'Each player submits one card from their hand of 26 cards. Whichever of the cards has higher value wins the round. First player to retain all 52 cards wins the game.']
-
-// function help () {
-//     document.getElementById('rules').innerHTML = instructions[0];
-// }
-
-// function resetHelp() {
-//     instructions.shift('')
-// }
 
 
-// let compSet, mySet = []
 
-// if(mySet.length === 0 || compSet.length === 0) {
-//     // game over
-//     if( playerDeck.length > 0 ){
-//       console.log('Player Won');
-//     } else {
-//       console.log('CPU Won');
-//     }
-//     return false;
-//   }
+
+
+
+
